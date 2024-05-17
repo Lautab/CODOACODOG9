e94eb3c8a4af49eb06a47a827cb9f10efff47f41
Lautab <119816103+Lautab@users.noreply.github.com>
2024-05-17 16:24:39 -0300

    Cambios en turnos

--- js/turnos.js
+++ js/turnos.js
@@ -1,10 +1,40 @@
 const butSig = document.getElementById('butSeguir')
+const butVolver = document.getElementById('butVolver')
+const butEnviar = document.getElementById('butEnviar')
+const containerBtns = document.getElementById('containerButtons')
 
 const divUno = document.getElementById('containerFormUno')
 const divDos = document.getElementById('containerFormDos')
 
 
+let etapa = 1
+
 butSig.addEventListener('click',()=>{
-    divUno.style.animation = 'none';
-    divUno.style.animation = 'ocultar 1s linear 0s 1 forwards normal;'
+    divUno.classList.remove('mostrar')
+    divUno.classList.add('ocultar')
+    divDos.classList.remove('ocultar')
+    butSig.classList.remove('mostrar')
+    butSig.classList.add('ocultar')
+    containerBtns.classList.remove('ocultar')
+
+    setTimeout(() => {
+        divDos.classList.add('mostrar')
+        containerBtns.classList.add('mostrar')
+    }, 950);
+    etapa = 2
 })
+
+butVolver.addEventListener('click', ()=>{
+    divDos.classList.remove('mostrar')
+    divDos.classList.add('ocultar')
+    containerBtns.classList.remove('mostrar')
+    containerBtns.classList.add('ocultar')
+    setTimeout(() => {
+        divUno.classList.remove('ocultar')
+        butSig.classList.remove('ocultar')
+        divUno.classList.add('mostrar')
+        butSig.classList.add('mostrar')
+    }, 950);
+    etapa = 1
+})
+

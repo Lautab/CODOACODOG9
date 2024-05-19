7573cbe70a43691d1be6036b8aa4702f3c8e9dd9
Lautab <119816103+Lautab@users.noreply.github.com>
2024-05-19 01:16:37 -0300

    FIN TURNOS (no responsive)

--- js/turnos.js
+++ js/turnos.js
@@ -5,36 +5,149 @@ const containerBtns = document.getElementById('containerButtons')
 
 const divUno = document.getElementById('containerFormUno')
 const divDos = document.getElementById('containerFormDos')
+const divTres = document.getElementById('containerFormTres')
+const divCuatro = document.getElementById('containerFormCuatro')
+
+let datos_formulario = []
+
+function mostrar(etiqueta) {
+    etiqueta.classList.remove("ocultar")
+    etiqueta.classList.add('mostrar')
+}
+function ocultar(etiqueta) {
+    etiqueta.classList.add('ocultar')
+    etiqueta.classList.remove("mostrar")
+}
+
+function cambiarA(aOcultar, aMostrar, aEtapa) {
+    aOcultar.forEach(element => {
+        ocultar(element)
+    });
+    setTimeout(() => {
+        aMostrar.forEach(element => {
+            sacarClass(element, 'oculto')
+            mostrar(element)
+        })
+    }, 950);
+    etapa = aEtapa
+}
+
+function sacarClass(etiqueta, clase) {
+    etiqueta.classList.remove(clase)
+}
+
+
+function guardarDatos(datos_formulario) {
+
+    const formData = new FormData(document.getElementById('formTurnos'));
+    for (var pair of formData.entries()) {
+        datos_formulario.push(pair[0] + '%%' + pair[1])
+    }
+    return datos_formulario
+}
+
+function mostrarDatos(datos_formulario) {
+    const displays = [
+        document.getElementById('form_tres_fecha'),
+        document.getElementById('form_tres_hora'),
+        document.getElementById('form_tres_tel'),
+        document.getElementById('form_tres_mail')
+    ]
+    const info = [
+        datos_formulario[6].split('%%')[1],
+        datos_formulario[7].split('%%')[1] + ' hs',
+        datos_formulario[2].split('%%')[1],
+        datos_formulario[3].split('%%')[1]
+    ]
+
+    for (let i = 0; i < 4; i++) {
+        displays[i].textContent = info[i]
+    }
+}
 
 
 let etapa = 1
 
-butSig.addEventListener('click',()=>{
-    divUno.classList.remove('mostrar')
-    divUno.classList.add('ocultar')
-    divDos.classList.remove('ocultar')
-    butSig.classList.remove('mostrar')
-    butSig.classList.add('ocultar')
-    containerBtns.classList.remove('ocultar')
-
-    setTimeout(() => {
-        divDos.classList.add('mostrar')
-        containerBtns.classList.add('mostrar')
-    }, 950);
-    etapa = 2
+butSig.addEventListener('click', () => {
+    if (etapa == 1) {
+        cambiarA([divUno, butSig], [divDos, containerBtns], 2)
+    }
 })
 
-butVolver.addEventListener('click', ()=>{
-    divDos.classList.remove('mostrar')
-    divDos.classList.add('ocultar')
-    containerBtns.classList.remove('mostrar')
-    containerBtns.classList.add('ocultar')
-    setTimeout(() => {
-        divUno.classList.remove('ocultar')
-        butSig.classList.remove('ocultar')
-        divUno.classList.add('mostrar')
-        butSig.classList.add('mostrar')
-    }, 950);
-    etapa = 1
+butVolver.addEventListener('click', () => {
+    if (etapa == 2) {
+        cambiarA([divDos, containerBtns], [divUno, butSig], 1)
+    }
+    else if (etapa == 3) {
+        cambiarA([divTres], [divDos], 2)
+    }
 })
 
+butEnviar.addEventListener('click', (e) => {
+    if (etapa == 2) {
+        datos_formulario = []
+        guardarDatos(datos_formulario)
+        let form_verificado = true
+        for (i = 0; i < 4; i++) {
+            try {
+                for (let index = 0; index < 8; index++) {
+                    if (datos_formulario[index].split("%%").length == 2) {
+                        form_verificado = true
+                    }
+                }
+            } catch (error) {
+                form_verificado = false
+            }
+        }
+        if (!form_verificado) {
+            alert('Faltan datos por completar para enviar la solicitud')
+        } else if (form_verificado) {
+            cambiarA([divDos], [divTres], 3)
+            mostrarDatos(datos_formulario)
+        }
+    }
+    else if (etapa == 3) {
+        cambiarA([divTres, containerBtns], [divCuatro], 4)
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    if (e.key == 'Enter') {
+        if (etapa == 1) {
+            cambiarA([divUno, butSig], [divDos, containerBtns], 2)
+        }
+        else if (etapa == 2) {
+            datos_formulario = []
+            guardarDatos(datos_formulario)
+            let form_verificado = true
+            for (i = 0; i < 4; i++) {
+                try {
+                    for (let index = 0; index < 8; index++) {
+                        if (datos_formulario[index].split("%%").length == 2) {
+                            form_verificado = true
+                        }
+                    }
+                } catch (error) {
+                    form_verificado = false
+                }
+            }
+            if (!form_verificado) {
+                alert('Faltan datos por completar para enviar la solicitud')
+            } else if (form_verificado) {
+                cambiarA([divDos], [divTres], 3)
+                mostrarDatos(datos_formulario)
+            }
+        }
+        else if (etapa == 3) {
+            cambiarA([divTres, containerBtns], [divCuatro], 4)
+        }
+    }
+    if (e.key == 'Escape'){
+        if (etapa == 2) {
+            cambiarA([divDos, containerBtns], [divUno, butSig], 1)
+        }
+        else if (etapa == 3) {
+            cambiarA([divTres], [divDos], 2)
+        }
+    }
+})
